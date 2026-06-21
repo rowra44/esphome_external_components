421441a4cfa0d85186f4e7e02a0ab6dda1f4b3de
Mark <34800035+rowra44@users.noreply.github.com>
2026-06-21 11:11:06 +0200

    Update gatepro_text_sensor.h

--- components/gatepro/gatepro_text_sensor.h
+++ components/gatepro/gatepro_text_sensor.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "esphome/core/component.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
 namespace esphome {
 namespace gatepro {
 
-class GateProTextSensor : public text_sensor::TextSensor {
+class GateProTextSensor : public Component, public text_sensor::TextSensor {
    public:   
 };
 
